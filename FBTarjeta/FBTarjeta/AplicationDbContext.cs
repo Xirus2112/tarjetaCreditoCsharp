f59d13d4ea274bea94d5297fef573a985ce63a74
Xirus2112 <66229925+Xirus2112@users.noreply.github.com>
2021-10-04 21:22:08 -0500

    Add files via upload

---
+++ FBTarjeta/FBTarjeta/AplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using FBTarjeta.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FBTarjeta
+{
+    public class AplicationDbContext: DbContext // crea una instancia en la base de datos
+    {
+        DbSet<TarjetaCredito> TarjetaCredito { get; set; }
+        public AplicationDbContext(DbContextOptions<AplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
